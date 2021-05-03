c57b83e76edeac1e87f765898534b3b468a8e44c
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-05-03 11:47:54 +0530

    background color succes

--- main.js
+++ main.js
@@ -9,20 +9,29 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "please select number!";
+
+    //when player wins
   } else if (guess === secretNumber) {
     if (score < 20) {
       document.querySelector(".message").textContent = "fucking correct";
       score++;
       document.querySelector(".score").textContent = score;
+
+      document.querySelector("body").style.backgroundColor = "#60b347";
     }
+
+    //when player guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector(".message").textContent = "too high";
       score--;
       document.querySelector(".score").textContent = score;
     }
+
+    //when player guess is too low
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector(".message").textContent = "too low";
@@ -31,6 +40,7 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   }
 
+  //when player loss
   if (score === 0) {
     document.querySelector(".message").textContent = "you lost";
   }
